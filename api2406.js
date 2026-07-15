89f97187e197e7c535b75d639a9596d9f303413e
tezorinoceronte <tezorinoceronte@gmail.com>
2026-07-15 14:50:33 -0600

    Update api2406.js

--- api2406.js
+++ api2406.js
@@ -21,6 +21,7 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 }));
+app.options('*', cors());
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
