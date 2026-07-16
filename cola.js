5ac5ec7601d061f68de34f56596a5c9585b3f9a2
tezorinoceronte <tezorinoceronte@gmail.com>
2026-07-16 01:21:08 -0600

    Update cola.js

--- cola.js
+++ cola.js
@@ -2,7 +2,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.SUPABASE_URL,
   ssl: { rejectUnauthorized: false },
   family: 4
 });
