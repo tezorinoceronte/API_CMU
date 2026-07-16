59fae6ae005a8d09c648133784642edb686bdc8d
tezorinoceronte <tezorinoceronte@gmail.com>
2026-07-16 02:35:58 -0600

    Update cola.js

--- cola.js
+++ cola.js
@@ -1,12 +1,8 @@
 const { Pool } = require('pg');
-require('dotenv').config();
-
-const connectionString = process.env.SUPABASE_URL || "postgresql://postgres.srfsdnphgdwrqjggcwfc:xSHxtYhf6rh6uXVx@aws-0-us-east-1.pooler.supabase.com:6543/postgres";
-
 const pool = new Pool({
-  connectionString: connectionString,
+  connectionString: process.env.SUPABASE_URL,
   ssl: { rejectUnauthorized: false },
-  family: 4
+  family: 4,
+  max: 5
 });
-
 module.exports = pool;
