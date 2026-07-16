fcb73647c1d37bfdea0d993983d9eb08a9c673d1
tezorinoceronte <tezorinoceronte@gmail.com>
2026-07-16 01:47:11 -0600

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 // Configuración robusta para evitar el error de red1
 const pool = new Pool({
-  connectionString: process.env.SUPAABASE_URL,
+  connectionString: process.env.SUPABASE_URL,
   ssl: {
     rejectUnauthorized: false
   },
