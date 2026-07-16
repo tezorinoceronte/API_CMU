d2fdd0d5b169582e4d315d536247c951533d18a3
tezorinoceronte <tezorinoceronte@gmail.com>
2026-07-15 22:14:51 -0600

    Add files via upload

---
+++ render-build.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -o errexit
+npm install
+apt-get update && apt-get install -y libnss3 libatk1.0-0 libatk-bridge2.0-0 libcups2 libdrm2 libxkbcommon0 libxcomposite1 libxdamage1 libxfixes3 libxrandr2 libgbm1 libasound2
