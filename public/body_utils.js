f4cfe1f6d2613805fde9d648c1fd25e11365e690
tezorinoceronte <tezorinoceronte@gmail.com>
2026-07-16 08:30:29 -0600

    Update body_utils.js

--- public/body_utils.js
+++ public/body_utils.js
@@ -30,7 +30,7 @@ if (numInput && btnValidar) {
 
 // --- FUNCIÓN PRINCIPAL DE CONSULTA ---
 // --- FUNCIÓN PRINCIPAL DE CONSULTA (CON LOGS DE DEPURACIÓN) ---
-async function validarForce() {
+async function validarforce() {
     const input = document.getElementById('numInput');
     const btn = document.getElementById('btnValidar');
     const alertBox = document.getElementById('alertBox');
