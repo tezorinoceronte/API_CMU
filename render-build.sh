9ef5ab3b66b81ebce23cb90b6fa54f6b6b70ecca
tezorinoceronte <tezorinoceronte@gmail.com>
2026-07-15 22:29:05 -0600

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,4 +1,21 @@
 #!/usr/bin/env bash
+# Salir inmediatamente si hay un error
 set -o errexit
+
+# Instalar dependencias de Node.js
 npm install
-apt-get update && apt-get install -y libnss3 libatk1.0-0 libatk-bridge2.0-0 libcups2 libdrm2 libxkbcommon0 libxcomposite1 libxdamage1 libxfixes3 libxrandr2 libgbm1 libasound2
+
+# Instalar dependencias necesarias para que Chrome/Puppeteer funcione en Linux
+apt-get update && apt-get install -y \
+    libnss3 \
+    libatk1.0-0 \
+    libatk-bridge2.0-0 \
+    libcups2 \
+    libdrm2 \
+    libxkbcommon0 \
+    libxcomposite1 \
+    libxdamage1 \
+    libxfixes3 \
+    libxrandr2 \
+    libgbm1 \
+    libasound2
