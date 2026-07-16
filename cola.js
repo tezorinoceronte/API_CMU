e0644e379000c01c3f7eb823b41b304c35d69c45
tezorinoceronte <tezorinoceronte@gmail.com>
2026-07-16 01:42:07 -0600

    Update cola.js

--- cola.js
+++ cola.js
@@ -1,11 +1,14 @@
-const { Pool } = require('pg'); 
+const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: process.env.SUPABASE_URL,
+  // Asegúrate de usar la clave exacta que está en el panel de Render
+  connectionString: process.env.SUPABASE_URL, 
   ssl: { rejectUnauthorized: false },
-  // Fuerza a usar IPv4
-  host: 'db.srfsdnphgdwrqjggcwfc.supabase.co', // Asegúrate de extraer solo el host aquí
-  family: 4 
+  family: 4, 
+  connectionTimeoutMillis: 15000,
+  idleTimeoutMillis: 30000,
+  max: 5
 });
 
 module.exports = pool;
